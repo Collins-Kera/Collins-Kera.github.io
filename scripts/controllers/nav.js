76289fb43ea31dd0223859b7cefe83c304664788
sunshinedesign36 <runn3rgirl36@gmail.com>
2015-04-08 03:48:36 -0600

    logout

---
+++ scripts/controllers/nav.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angbaseApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the angbaseApp
+ */
+angular.module('angbaseApp')
+  .controller('NavCtrl', function ($scope, Auth, $location) {
+    if(Auth.getUser()){
+      $scope.loggedIn = true;
+    }
+
+    console.log($scope.loggedIn);
+    $scope.logout = function() {
+      Auth.logout().then(redirectHome);
+    }
+    function redirectHome() {
+      $location.path('/');
+    }
+  });
