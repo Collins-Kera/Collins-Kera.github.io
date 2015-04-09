3b4f48112039e2df8740983f1725415b704e7ccb
sunshinedesign36 <runn3rgirl36@gmail.com>
2015-04-09 01:08:17 -0600

    no form

--- scripts/controllers/contact.js
+++ scripts/controllers/contact.js
@@ -8,8 +8,19 @@
  * Controller of the angbaseApp
  */
 angular.module('angbaseApp')
-  .controller('ContactCtrl', function ($scope, Auth, $location ) {
-    // $scope.clearForm = function() {
-    //   $scope.err = "Thank you.";
-    // }
+  .controller('ContactCtrl', function ($scope, $firebaseArray, Auth, $location ) {
+    var ref = new Firebase("https://sweltering-torch-2482.firebaseio.com/messages");
+
+    //create a synchronized array
+    $scope.messages = $firebaseArray(ref);
+    
+    //add new message
+    $scope.submitForm = function(name, email, phone, text) {
+      $scope.messages.$add({
+        name: name,
+        email: email,
+        phone: phone,
+        text: text
+      })
+    }
   });
